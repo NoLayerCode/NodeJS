e97aec536c610a270baf60f5cd565e093274885f
NoLayerCode <vyelhekar@gmail.com>
2019-07-30 21:05:51 +0530

    Adding routes

--- Basics/Express-firstApp/routes/index.js
+++ Basics/Express-firstApp/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/', function(req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.get('/detail', function(req, res, next) {
+    res.send('details');
 });
 
 module.exports = router;
