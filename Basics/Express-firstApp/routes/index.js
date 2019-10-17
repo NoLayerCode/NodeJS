641ec67ac238160bc52c036f256b24329a299056
NoLayerCode <vyelhekar@gmail.com>
2019-10-18 02:10:49 +0530

    handlebars if conditions

--- Basics/Express-firstApp/routes/index.js
+++ Basics/Express-firstApp/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Cool, Dude!', condition: false, anArray: [1, 2, 4] });
+    res.render('index', { title: 'Cool, Dude!', condition: true, anArray: [1, 2, 4] });
 });
 
 module.exports = router;
