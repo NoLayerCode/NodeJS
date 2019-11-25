e0a5439b4df15f644a83e9c49912534eea8f5c62
NoLayerCode <vyelhekar@gmail.com>
2019-11-26 00:22:21 +0530

    added get meathod middleware

--- Basics/get-post-req/routes/index.js
+++ Basics/get-post-req/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
     res.render('index', { title: 'Cool, Dude!', condition: true, anArray: [1, 2, 4] });
 });
 
+router.get('/test/:id', function(req, res, next) {
+    res.render('test', { output: req.params.id });
+});
+
 module.exports = router;
